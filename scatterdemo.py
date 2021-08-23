44841fd2f1f07df4730b60673e531fe0f748d720
Chris <ChrisyangHFNU@gmail.com>
2021-08-23 13:34:23 +0800

    create scatterdemo.py

---
+++ scatterdemo.py
@@ -0,0 +1,15 @@
+﻿import matplotlib.pyplot as plt
+
+height = [161,170,182,175,173,165]
+weight = [50,58,80,70,69,55]
+
+# 画图
+plt.scatter(height,weight,alpha=0.7)
+# 设置X轴标签
+plt.xlabel('height')
+# 设置Y轴标签
+plt.ylabel('weight')
+# 添加图的标题
+plt.title('scatter demo')
+# 图形显示
+plt.show()
