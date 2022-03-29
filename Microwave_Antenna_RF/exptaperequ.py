f285b6cfb1724ad5bdbef987d743977f7f21074e
Chris <chrisyanghfnu@gmail.com>
2022-03-29 17:56:21 +0800

    change math.exp to np.exp

--- Microwave_Antenna_RF/exptaperequ.py
+++ Microwave_Antenna_RF/exptaperequ.py
@@ -4,11 +4,9 @@ import numpy as np
 zin = 100 # unit:Ohm
 zout = 50
 freqLow = 1e9
-quarterwl = 0.25*187 # 波导波长 70.7Ohm, microstrip quarter wavelength, epsilon 3.38, frequency,1GHz
+quarterwl = 0.25*187 # 波导波长 从50ohm到100ohm，接1/4波长传输线70.7Ohm, microstrip quarter wavelength, epsilon 3.38, frequency,1GHz
 a = math.log(zout/zin)/quarterwl
-x = np.linspace(0,quarterwl,8)
-z = np.zeros([1,8])
-exp(a*x)
-print(zin*math.exp(a*x))
+x = np.linspace(0,quarterwl,10)
+z = zin*np.exp(a*x)
 
 print(z)
