2badf66deac222a6d5df671bbb1e473cc54c38bb
Chris <chrisyanghfnu@gmail.com>
2023-12-01 15:07:41 +0800

    revise code

--- MATLAB/CEM/ShaWei/EFIE.m
+++ MATLAB/CEM/ShaWei/EFIE.m
@@ -27,10 +27,10 @@ circlelength=a*anglemin;  %  弧长
 
 %  坐标定位(角度)
 anglevalue=-anglemin/2;  %  角度初值
-for i=1:count;
+for i=1:count
     anglevalue=anglevalue+anglemin;  %  角度递增
     point(i)=anglevalue;  %  角度赋值
-end;
+end
 
 
 
@@ -38,7 +38,7 @@ end;
 for m=1:count  %  场点循环
     for n=1:count  %  源点循环  
         if (m==n)
-            l_mn(m,n)=(1-(2*j/pi)*log(EULER*k*circlelength/(4*EX)))*circlelength*(k*Z/4);
+            l_mn(m,n)=(1-(2*1i/pi)*log(EULER*k*circlelength/(4*EX)))*circlelength*(k*Z/4);
             %  奇异点处理
         else
             fieldpoint=point(m);   %  场点
@@ -46,7 +46,7 @@ for m=1:count  %  场点循环
    
             % %  被积函数
             integerfunction1=2*k*a*abs(sin((currentpoint-fieldpoint)/2));
-            integerfunction2=besselj(0,integerfunction1)-j*bessely(0,integerfunction1);
+            integerfunction2=besselj(0,integerfunction1)-1i*bessely(0,integerfunction1);
             % % 元素赋值
             l_mn(m,n)=(circlelength*k*Z/4)*integerfunction2; %  调用积分函数求解阻抗矩阵l_mn
         end  
@@ -56,9 +56,9 @@ end
   
 
 %  电压矩阵g_m确定
-for i=1:count;
-    g_m(i)=amplitude*exp(-j*k*a*cos(point(i)) );  %  往X方向传播的TM波
-end;
+for ii=1:count
+    g_m(ii)=amplitude*exp(-1i*k*a*cos(point(ii)) );  %  往X方向传播的TM波
+end
 
 
 
@@ -69,10 +69,10 @@ currentresult=l_mn\g_m.';  %  矩阵除法
 
 %  解析解求解
 initialcurrent=0;  %  求和初始化
-for m=1:count;
-    for n=-20:20;
+for m=1:count
+    for n=-20:20
         initialcurrent=initialcurrent+(2*amplitude/(2*pi*f*U0*pi*a))*...
-                       j^(-n)*exp(j*n*point(m))/(besselj(n,k*a)-j*bessely(n,k*a));  %  解析解电流
+                       1i^(-n)*exp(1i*n*point(m))/(besselj(n,k*a)-1i*bessely(n,k*a));  %  解析解电流
     end
         analysis(m)=initialcurrent;  %  赋值
         initialcurrent=0;  %  置零
@@ -82,24 +82,24 @@ end
 
 %   RCS求解
 ii=0;  %  矩阵下标
-for si=0:pi/180:2*pi;  %  散射角
+for si=0:pi/180:2*pi  %  散射角
     
     ii=ii+1;
     sum=0; %  求和初值
     
-    for seg=1:count;
-       sum=sum+currentresult(seg,1)*circlelength*exp( j*k*( a*cos(point(seg))*cos(si)+a*sin(point(seg))*sin(si) ) );
+    for seg=1:count
+       sum=sum+currentresult(seg,1)*circlelength*exp(1i*k*( a*cos(point(seg))*cos(si)+a*sin(point(seg))*sin(si) ) );
     end
     
     rcs(ii)=abs(sum).^2*k*Z^2/4;  %  RCS计算
-end;
+end
 
 rcs=10*log10(rcs*k/(2*pi));  %  归一化RCS
 
 
 
 %  图形显示
-figure(1);
+figure(1)
 plot(point*180/pi,abs(currentresult.'),'rx');  %  矩量法计算出的电流
 hold on;
 plot(point*180/pi,abs(analysis),'b');  %  解析法计算出的电流
@@ -109,6 +109,7 @@ title('MOM APPLIED IN CYLINDER SCATTERING PROBLEM');  %  题目
 xlabel('angle');  %  x坐标轴
 ylabel('current');  %  y坐标轴
 legend('mom result','analysis result');  %  标识
+hold off
         
 figure(2)
 plot([0:1:180],rcs(1:181));
