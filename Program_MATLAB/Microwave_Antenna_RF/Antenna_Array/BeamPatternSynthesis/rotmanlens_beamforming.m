062d176ea90134709a37b68eeca93baf94b62d60
chris <chrisyanghfnu@gmail.com>
2025-04-24 20:07:03 +0800

    Enhance rotmanlens_beamforming.m to include 9GHz beam analysis and improve comments for clarity

--- Program_MATLAB/Microwave_Antenna_RF/Antenna_Array/BeamPatternSynthesis/rotmanlens_beamforming.m
+++ Program_MATLAB/Microwave_Antenna_RF/Antenna_Array/BeamPatternSynthesis/rotmanlens_beamforming.m
@@ -1,4 +1,5 @@
-% 波束端口4 入射 天线端口的幅度相位分布,10GHz
+%% 7个波束天线端口的幅度相位分布，波束图-10 GHz
+
 clear;
 close all;
 
@@ -52,7 +53,7 @@ mag7 = 10.^(mag7_dB/10);
 x7 = [pha7 mag7];
 
 eleNum = 8;
-elespace = 0.5;
+elespace = 0.5; % 10GHz
 thetaRange = -90:90;
 fm1 = linearArrayfactor(eleNum,thetaRange,elespace,x1);
 fm2 = linearArrayfactor(eleNum,thetaRange,elespace,x2);
@@ -63,5 +64,28 @@ fm6 = linearArrayfactor(eleNum,thetaRange,elespace,x6);
 fm7 = linearArrayfactor(eleNum,thetaRange,elespace,x7);
 plot(thetaRange,20*log10(abs(fm1)),'k-.',thetaRange,20*log10(abs(fm2)),'k--',thetaRange,20*log10(abs(fm3)),'k-',thetaRange,20*log10(abs(fm4)),'k-*',...
     thetaRange,20*log10(abs(fm5)),'b-',thetaRange,20*log10(abs(fm6)),'b--',thetaRange,20*log10(abs(fm7)),'b-.','LineWidth',2);
+xlim([-90 90]);
+ylim([-40 0]);
+
+%% 观察中心波束在10GHz 和 9GHz 两个频点上波束有没有发生偏移
+tmp41_9ghz = csvread('phaB4.csv',1,0);
+pha4_9ghz = tmp41_9ghz(1,2:end);
+tmp42_9ghz = csvread('magB4.csv',1,0);
+mag4_dB_9ghz = tmp42_9ghz(1,2:end);
+mag4_9ghz = 10.^(mag4_dB_9ghz/10);
+x4_9ghz = [pha4_9ghz mag4_9ghz];
+
+tmp11_9ghz = csvread('phaB1.csv',1,0);
+pha1_9ghz = tmp11_9ghz(1,2:end);
+tmp12_9ghz = csvread('magB1.csv',1,0);
+mag1_dB_9ghz = tmp12_9ghz(1,2:end);
+mag1_9ghz = 10.^(mag1_dB_9ghz/10);
+x1_9ghz = [pha1_9ghz mag1_9ghz];
+
+elespace_9ghz = 9/10*0.5; % 相对于9GHz 的波长
+fm1_9ghz = linearArrayfactor(eleNum,thetaRange,elespace_9ghz,x1_9ghz);
+fm4_9ghz = linearArrayfactor(eleNum,thetaRange,elespace_9ghz,x4_9ghz);
+figure
+plot(thetaRange,20*log10(abs(fm1)),'b-',thetaRange,20*log10(abs(fm1_9ghz)),'b--',thetaRange,20*log10(abs(fm4)),'k-',thetaRange,20*log10(abs(fm4_9ghz)),'k--','LineWidth',2);
 xlim([-90 90]);
 ylim([-40 0]);
